8632fa7d03fff8df982a36f2b1a70944987c2108
Justin Feng <realfengjia@foxmail.com>
2023-05-24 14:08:02 +0800

    Try coverage test

--- hardhat.config.js
+++ hardhat.config.js
@@ -15,10 +15,10 @@ const SEPOLIA_PRIVATE_KEY = "YOUR SEPOLIA PRIVATE KEY";
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.18",
-  networks: {
-    sepolia: {
-      url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
-      accounts: [SEPOLIA_PRIVATE_KEY]
-    }
-  },
+  // networks: {
+  //   sepolia: {
+  //     url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
+  //     accounts: [SEPOLIA_PRIVATE_KEY]
+  //   }
+  // },
 };
